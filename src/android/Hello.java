0d5eb9fbd147dc6d379934ce2142ba2d63896198
yonatan <yonatan.py@gmail.com>
2015-06-27 13:24:19 +0300

    added logging

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -3,14 +3,17 @@ package com.example.plugin;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import android.util.Log;
 
 public class Hello extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-
+        Log.d("HELLO", "in execute");
+        Log.d( "action:" );
+        Log.d( action );
         if (action.equals("greet")) {
-
+            
             String name = data.getString(0);
             String message = "Hello, " + name;
             callbackContext.success(message);
